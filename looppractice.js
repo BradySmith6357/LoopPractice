a3eccf36fd17384ab1bf789c3fb6a95a70e6bf2e
Student <student@RU-023.local>
2016-01-28 14:42:33 -0700

    Woreked on LoopPractice2

---
+++ looppractice.js
@@ -0,0 +1,59 @@
+// first solution for #1
+// for (var 1 = 0; i <= 5; i++){
+	// console.log (i * 100);
+// }
+// better solution for #1
+// for (var i = 0; i <600; i+=100){
+// 	console.log(i);
+// }
+// // #2
+// for (var i = 1; i < 65; i *= 2){
+// 	console.log(i);
+// }
+// // #3
+// for (var i = 1; i <= 3; i++) {
+// 	for (var j = 1; j <= 3; j++)
+// 	console.log(i);
+// }
+// // #4
+// for (var i = 0; i <= 10; i+=2){
+// 	console.log(i);
+// }
+// // #5
+// for (var i = 0; i <= 15; i+=3){
+// 	console.log(i);
+// }
+// // #6
+// for (var i = 9; i >= 0; i--){
+// 	console.log(i);
+// }
+// // #7
+// for (var i = 0; i <= 2; i++){
+// 	for (var j = 0; j <= 3; j++)
+// 		console.log(j);
+// }
+
+// LOOP PRACTICE #2
+
+var animals = ["rat", "cat", "butterfly", "marmot", "ocelot"]
+
+// #1
+// for (var i=0; i < animals.length; i++) {
+// 	console.log(animals[i])
+// 	animals.splice(4)
+// }
+
+// #2 
+// for (var i = 0; i < animals.length; i+=2) {
+// 	console.log(animals[i])
+// }
+
+// #3 This currently show first console.log as undefined. Why?
+// for (var i = animals.length; i >= 0; i--) {
+// 	console.log(animals[i])
+// }
+
+// #4 Have not solved
+// for (var i = 0; i < animals.length; i++) {
+// 	console.log(animals[i])
+// }
